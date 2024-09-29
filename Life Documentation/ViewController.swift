13dc4e43388abe4bc62dee80db1842764f1da761
Rabbit <labcgood44@gmail.com>
2024-09-29 16:36:45 +0800

    Initial Commit

---
+++ Life Documentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life Documentation
+//
+//  Created by Labe on 2024/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
